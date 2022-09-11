dd02b97917abf4585525641adcccf19e8fe21ec8
Alina <nikitina@terpmail.umd.edu>
2022-09-11 12:29:05 -0400

    adding banking system project

---
+++ src/com/example/bank/Customer.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class Customer {
+    String[] name;
+    int id;
+    double startingBalance;
+    Transaction[] transactions;
+
+    public Customer(String firstName, String lastName, int id, double startingBalance) {
+
+    }
+
+}
